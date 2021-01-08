42e653857ca92d0fa7b7bbe0f0ab8197a21c7d47
pastorek89 <pastor.patryk@gmail.com>
2021-01-08 19:39:47 +0100

    First commit Spring Boot

---
+++ src/main/java/com/exercises/ExercisesKodillaApplication.java
@@ -0,0 +1,13 @@
+package com.exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesKodillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesKodillaApplication.class, args);
+	}
+
+}
